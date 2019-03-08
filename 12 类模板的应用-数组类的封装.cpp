4155f08e4797a3e71a648148a8614ce39f107712
phoenixfate <sm516116978@outlook.com>
2019-03-08 23:53:10 +0800

    finished by mac

--- 12 类模板的应用-数组类的封装.cpp
+++ 12 类模板的应用-数组类的封装.cpp
@@ -46,12 +46,12 @@ public:
         }
     }
 
-    MyArray operator[](int index)
+    T& operator[](int index)
     {
         return this->address[index];
     }
 
-    void pushBack(T &value)
+    void pushBack(T& value)
     {
         this->address[this->size]=value;
         ++this->size;
@@ -73,11 +73,6 @@ private:
     int size;
 };
 
-//template <class T>
-//ostream& operator<<(ostream&cout,T &myArray)
-//{
-//    cout<<myArray<<endl;
-//}
 
 void test1201()
 {
@@ -92,9 +87,49 @@ void test1201()
     }
 }
 
+class Person12
+{
+public:
+    Person12(){};
+    Person12(string name,int age):name(name),age(age)
+    {
+
+    }
+    string name;
+    int age;
+};
+
+
+void printPersonArray(MyArray<Person12> &arr)
+{
+    for(int i=0;i<arr.getSize();++i)
+    {
+        cout<<arr[i].name<<"  "<<arr[i].age<<endl;
+    }
+}
+
+void test1202()
+{
+    Person12 p1("a",1);
+    Person12 p2("b",2);
+    Person12 p3("c",3);
+    Person12 p4("d",4);
+
+    MyArray<Person12> arr(10);
+    arr.pushBack(p1);
+    arr.pushBack(p2);
+    arr.pushBack(p3);
+
+    printf("%p\n",&p1);
+    printf("%p\n",&arr[0]);
+
+    printPersonArray(arr);
+}
+
 int main()
 {
     test1201();
+    test1202();
     return 0;
 }
 
